24f1f6d263caaa6e7d64f12e65174c0e86389162
Atílio Antônio <atiliodadalto@hotmail.com>
2018-08-21 11:42:00 -0300

    Ex015

--- Ex015.c
+++ Ex015.c
@@ -1,10 +1,21 @@
-//
+// o) Faça um programa que mostre se um número inteiro é par ou ímpar.
 
 #include <stdio.h>
 #include <stdlib.h>
 
 int main()
 {
+    int n;
+    printf("Informe um numero inteiro: ");
+    scanf("%i", &n);
+    if (n % 2 == 0)
+    {
+        printf("O numero eh par");
+    }
+    else
+    {
+        printf("O numero eh impar");
+    }
 
     return 0;
 }
