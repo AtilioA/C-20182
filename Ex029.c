91a0fb8e6da492c362643f636e056bd5c9d2c6b5
Atílio Antônio <atiliodadalto@hotmail.com>
2018-08-30 12:04:54 -0300

    Atleta

--- Ex029.c
+++ Ex029.c
@@ -1,10 +1,30 @@
-//
+// Faça um algoritmo que leia a idade de um atleta e escreva em que categoria ele se
+// enquadra, seguindo o quadro
 
 #include <stdio.h>
 #include <stdlib.h>
 
 int main()
 {
+    int idade;
+
+    printf("Informe a idade do atleta: ");
+    scanf("%i", &idade);
+
+    if (idade < 5)
+        printf("Atleta sem categoria - muito novo.");
+
+    else if (idade <= 10)
+        printf("Atleta infantil.");
+
+    else if (idade <= 17)
+        printf("Atleta juvenil.");
+
+    else if (idade <= 30)
+        printf("Atleta profissional.");
+
+    else if (idade > 30)
+        printf("Atleta senior.");
 
     return 0;
 }
