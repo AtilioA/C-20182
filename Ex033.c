28c29c407672315d4d77a92f2e3c975bdb7a5f25
AtilioA <atiliodadalto@hotmail.com>
2018-08-30 14:57:29 -0300

    LabGrad

--- Ex033.c
+++ Ex033.c
@@ -1,10 +1,24 @@
-//
+// Escreva um programa que solicite dois caracteres de A a Z ao usuário e imprima o
+// número de caracteres existentes entre eles. Exemplo:
 
 #include <stdio.h>
 #include <stdlib.h>
 
 int main()
 {
+    int numerochar = 0;
+    char c1, c2;
+
+    printf("Informe o primeiro caractere: ");
+    scanf("%c", &c1);
+
+    printf("Informe o segundo caractere: ");
+    scanf(" %c", &c2);
+
+    if (c1 != c2)
+        numerochar = abs(c1 - c2) - 1;
+
+    printf("Numero de caracteres existentes entre '%c' e '%c': %i.\n", c1, c2, numerochar);
 
     return 0;
 }
--- Ex034.c
+++ Ex034.c
@@ -1,3 +1,6 @@
+// 3) Fazer um programa que leia vários números (um por vez) e imprima 0 (zero) se o número
+// não for múltiplo de 2, 3, 5 e 7. Imprimir 1 (um) se o número for múltiplo de 2 ou 3 e 2
+// (dois) se o número for múltiplo de 5 e 7. Sair do programa quando for digitado o valor -1
 //
 
 #include <stdio.h>
