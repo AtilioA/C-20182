c507696f0a33e59a62d498348931648630e0a630
Atílio Antônio <atiliodadalto@hotmail.com>
2018-08-31 10:42:00 -0300

    Listas 1 e 2

--- Ex024.c
+++ Ex024.c
@@ -1,26 +1,10 @@
-// Escreva um programa para calcular o valor de π de acordo com a
-// seguinte fórmula:
-// π =√(n ∑ k=1 | 6/k^2)
+// pq ta vazio
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
 
 int main()
 {
-    int n, k;
-    float r, s = 0;
-
-    printf("Informe o valor de n: ");
-    scanf("%i", &n);
-
-    for(k = 1; k <= n; ++k)
-    {
-        r = (6 / (pow(k, 2)));
-        s += r;
-    }
-    
-    printf("%.6f", sqrt(s));
 
     return 0;
 }
---
+++ Ex026.c
@@ -0,0 +1,26 @@
+// Escreva um programa para calcular o valor de π de acordo com a
+// seguinte fórmula:
+// π =√(n ∑ k=1 | 6/k^2)
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main()
+{
+    int n, k;
+    float r, s = 0;
+
+    printf("Informe o valor de n: ");
+    scanf("%i", &n);
+
+    for(k = 1; k <= n; ++k)
+    {
+        r = (6 / (pow(k, 2)));
+        s += r;
+    }
+    
+    printf("%.6f", sqrt(s));
+
+    return 0;
+}
