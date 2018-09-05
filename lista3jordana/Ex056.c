a552121eb2ad44d8c162b5879b27eaa049f5b624
Atílio Antônio <atiliodadalto@hotmail.com>
2018-09-05 20:51:06 -0300

    Update Ex056.c

--- lista3jordana/Ex056.c
+++ lista3jordana/Ex056.c
@@ -1,4 +1,4 @@
-// 6. Faça um programa que simula o lançamento de dois dados, d1 e d2, n vezes, e tem como
+// 7. Faça um programa que simula o lançamento de dois dados, d1 e d2, n vezes, e tem como
 // saída o número de cada dado e a relação entre eles (>,<,=) de cada lançamento.
 
 #include <stdio.h>
@@ -6,7 +6,28 @@
 
 int main()
 {
-    int ;
+    int d1, d2, i, n;
+
+    printf("Informe o numero de vezes: ");
+    scanf("%i", &n);
+
+    for(i = 0; i < n; i++)
+    {
+    printf("d1: ");
+    scanf("%i", &d1);
+
+    printf("d2: ");
+    scanf("%i", &d2);
+
+    if (d1 > d2)
+        printf("d1 (%i) > d2 (%i)\n", d1, d2);
+        
+    else if (d1 < d2)
+        printf("d1 (%i) < d2 (%i)\n", d1, d2);
+
+    else
+        printf("d1 (%i) = d2 (%i)\n", d1, d2);
+    }
 
     return 0;
 }
