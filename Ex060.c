b2fe4f950324a28c56525d11c6708dac42bcc782
AtilioA <atiliodadalto@hotmail.com>
2018-09-27 14:59:21 -0300

    ahusdahusd

---
+++ Ex060.c
@@ -0,0 +1,32 @@
+// 2) Elabore uma função que permita somar os elementos de um vetor de inteiros.
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int soma_vetor(int n[])
+{
+    int soma = 0, i;
+
+    for(i = 0; i < 10; i++)
+    {
+        soma += n[i];
+    }
+
+    return soma;
+}
+
+
+int main()
+{
+    int n[10], i;
+
+    printf("Informe os numeros do vetor: ");
+    for(i = 0; i < 10; i++)
+    {
+        scanf("%i", &n[i]);
+    }
+
+    printf("%i.\n", soma_vetor(n));
+
+    return 0;
+}
