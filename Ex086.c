e8fd530081bea1ba6e2da3472ee839454e816200
AtilioA <atiliodadalto@hotmail.com>
2018-10-05 17:45:00 -0300

    Lista 5 Jordana

--- Ex086.c
+++ Ex086.c
@@ -1,23 +1,59 @@
-/*
+/* 1) Implemente um programa Combiner que receba dois Strings como parâmetros e
+combine-os, alternando letras, começando com a primeira letra da primeira String,
+seguida pela primeira letra da segunda String, depois pela segunda letra da primeira
+String, etc. As letras restantes da String mais longa são então anexadas ao final da string
+de combinação e esta string de combinação é retornada. Cada string conterá entre 1 e
+50 caracteres.
 */
 
 #include <stdio.h>
 #include <string.h>
 
-int bosta(char);
+int maiorStr(char s1[], char s2[]);
+int printaAlternada(char s1[], char s2[], int lenMaior);
 
 int main()
 {
-    char frase[100];
-    int i;
+    char s1[50], s2[50];
+    int lenMaior;
 
-    printf("Informe uma string: ");
-    gets(frase);
+    printf("Informe a primeira string: ");
+    scanf("%s", s1);
+
+    printf("Informe a segunda string: ");
+    scanf("%s", s2);
+
+    lenMaior = maiorStr(s1, s2);
+
+    printaAlternada(s1, s2, lenMaior);
 
     return 0;
 }
 
-int bosta(char frase)
+int maiorStr(char s1[], char s2[])
 {
+    if (strlen(s1) >= strlen(s2))
+    {
+        return strlen(s1);
+    }
 
+    else
+    {
+        return strlen(s2);
+    }
+}
+
+int printaAlternada(char s1[], char s2[], int lenMaior)
+{
+    int i;
+
+    for (i = 0; i < lenMaior; i++)
+    {
+        printf("%c", s1[i]);
+        printf("%c", s2[i]);
+    }
+
+    printf("\n");
+
+    return 0;
 }
--- Ex087.c
+++ Ex087.c
@@ -1,23 +1,88 @@
-/*
+/* Júlio César usou um sistema de criptografia, agora conhecido como Cifra de César, que
+deslocava cada letra 2 lugares para frente pelo alfabeto (por exemplo, 'A' muda para 'C',
+'R' muda para 'T', etc.). No final do alfabeto nós damos a volta, isso é, 'Y' muda para 'A'.
+Podemos, é claro, tentar mudar qualquer número.
+Dado um número e uma string já codificada utilizando a cifra de César, decodifique-a e
+imprima a sentença original.
 */
 
 #include <stdio.h>
 #include <string.h>
 
-int bosta(char);
+int decifra(char frase[], int cifra);
 
 int main()
 {
-    char frase[100];
-    int i;
+    char frase[50];
+    int cifra;
 
-    printf("Informe uma string: ");
-    gets(frase);
+    printf("Informe o numero da cifra: ");
+    scanf("%i", &cifra);
+
+    printf("Informe uma string codificada: ");
+    scanf("%s", frase);
+
+    decifra(frase, cifra);
 
     return 0;
 }
 
-int bosta(char frase)
+int decifra(char frase[], int cifra)
 {
+    int tamanho, i, cifraVolta;
 
+    tamanho = strlen(frase);
+
+    for (i = 0; i < tamanho; i++)
+    {
+        if (distZ(frase[i]) < cifra)
+        {
+            cifraVolta = cifra - distZ(frase[i]);
+
+            if maiuscula(frase[i])
+            {
+                frase[i] = 'A';
+            }
+            else
+            {
+                frase[i] = 'a';
+            }
+        }
+    }
+
+    for (i = 0; i < tamanho; i++)
+    {
+        printf("%c", (frase[i] - cifra));
+    }
+
+    printf("\n");
+
+    return 0;
+}
+
+
+int distZ(char frase)
+{
+    if maiuscula(frase)
+    {
+        return frase - 'Z';
+    }
+
+    else
+    {
+        return frase - 'z';
+    }
+}
+
+int maiuscula(char frase)
+{
+    if (frase > 'a' && frase < 'z')
+    {
+        return 1;
+    }
+
+    else
+    {
+        return 0;
+    }
 }
