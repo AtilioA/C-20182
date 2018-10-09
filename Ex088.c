2c0c7686a5ca01abca056446b592ca0fb2ae4cdc
AtilioA <atiliodadalto@hotmail.com>
2018-10-09 17:41:46 -0300

    Matrizes

--- Ex088.c
+++ Ex088.c
@@ -4,20 +4,54 @@
 #include <stdio.h>
 #include <string.h>
 
-int bosta(char);
+int printaMat(int mat[][10]);
+int inicMat(int mat[][10], int linhas, int colunas);
 
 int main()
 {
-    char frase[100];
-    int i;
+    int i, j;
+    int matriz[2][2];
 
-    printf("Informe uma string: ");
-    gets(frase);
+    for (i = 0; i < 2; i++) 
+    {
+        for(j = 0; j < 2; j++)
+        {
+            matriz[i][j] = 0;
+        }
+    }
+
+    inicMat(matriz, 2, 2);
+    printaMat(matriz);
 
     return 0;
 }
 
-int bosta(char frase)
+int inicMat(int mat[][10], int linhas, int colunas)
 {
+    int i, j;
+    
+    for (i = 0; i < linhas; i++)
+    {
+        for (j = 0; j < colunas; j++)
+        {
+            mat[i][j] = i + j;
+        }
+    }
+    
+    return 0;
+}
 
+int printaMat(int mat[][10])
+{
+    int i, j;
+
+    for (i = 0; i < 2; i++) 
+    {
+        for(j = 0; j < 2; j++)
+        {
+            printf("m[%d][%d] = %d.\n", i, j, mat[i][j]);
+        }
+    }
+        
+    return 0;
 }
--- Ex089.c
+++ Ex089.c
@@ -4,20 +4,36 @@
 #include <stdio.h>
 #include <string.h>
 
-int bosta(char);
-
 int main()
 {
-    char frase[100];
-    int i;
+    int i, j, p, mat[2][2];
 
-    printf("Informe uma string: ");
-    gets(frase);
+    printf("Informe uma matriz:\n");
+    
+    for (i = 0; i <= 1; ++i)
+    {
+        printf("Informe a posicao [%d][%d]: ", i, j);
+        scanf("%i", &p);
+
+        mat[i][j] = p;
+
+        for (j = 1; j > 0; j--)
+        {
+            printf("Informe a posicao [%d][%d]: ", i, j);
+            scanf("%i", &p);
+
+            mat[i][j] = p;    
+        }
+    }
+
+    for (i = 0; i <= 1; ++i)
+    {
+        printf("%d \n", mat[i][j]);
+        for (j = 1; j > 0; j--)
+        {
+            printf("%d \n", mat[i][j]);
+        }
+    }
 
     return 0;
-}
-
-int bosta(char frase)
-{
-
 }
