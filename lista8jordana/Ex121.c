4e006e85337fa5aa61589358ca4716f8f10bd1ad
AtilioA <atiliodadalto@hotmail.com>
2018-12-01 20:34:27 -0200

    Lista 8 finalizada

--- lista8jordana/Ex121.c
+++ lista8jordana/Ex121.c
@@ -6,67 +6,154 @@ c) Crie dinamicamente e construa a matriz transposta N X M de inteiros. */
 #include <stdio.h>
 #include <stdlib.h>
 
-void mallocaMatriz(int ***M, int l, int c);
-void leMat(int **M, int l, int c);
+int *alocaLinha(int nLinhas);
+int **mallocaMatriz(int l, int c);
+void maiores(int **M, int *maiores, int *posicoes, int l, int c);
+int tresMaiores(int **M, int l, int c, int maior);
+void transpoeDinamica(int **mOriginal, int **mTransposta, int l, int c);
+void printaVet(int V[], int t);
 void printaMat(int **M, int l, int c);
+void leMat(int **M, int l, int c);
 
 int main()
 {
-    int **matriz;
-    int M, N;
+    int M, N, **matriz, **transposta, *maior, *posicoes;
 
     printf("Informe o tamanho da matriz [M x N]: ");
-    scanf("%i %i", &N, &M);
+    scanf("%i %i", &M, &N);
 
-    mallocaMatriz(&matriz, M, N);
+    matriz = mallocaMatriz(M, N); // opora
+    transposta = mallocaMatriz(N, M);
+
     leMat(matriz, M, N);
     printaMat(matriz, M, N);
 
+    transpoeDinamica(matriz, transposta, M, N);
+    printaMat(transposta, N, M);
+
+    maior = (int *)malloc(4 * sizeof(int));
+    posicoes = (int *)malloc(10 * sizeof(int));
+    maiores(matriz, maior, posicoes, M, N);
+
+    for (int i = 0; i < N; i++)
+    {
+        free(matriz[i]);
+    }
     free(matriz);
+    free(maior);
+    free(posicoes);
     return 0;
 }
 
-// vsf
-void mallocaMatriz(int ***M, int l, int c)
+int *alocaLinha(int l)
+{
+    int *linha;
+    linha = (int *)malloc(l * sizeof(int));
+
+    return linha;
+}
+
+int **mallocaMatriz(int l, int c)
 {
     int i;
 
-    **M = malloc(l * sizeof(int *));
+    int **matriz = malloc(c * sizeof(int *));
 
     for (i = 0; i < c; i++)
     {
-        *M[i] = (int *)malloc(c * sizeof(int));
+        matriz[i] = alocaLinha(l);
     }
+
+    return matriz;
 }
 
-
-// olha ta foda
-void maiores(int **M, int l, int c)
+void maiores(int **M, int *maiores, int *posicoes, int l, int c)
 {
-    int i, j, k, p = 0, *maiores, maior = 0, qtd = 3;
+    int i, j, k, maior, qtd = 0;
 
-    maiores[0] = M[0][0];
+    printf("foi");
 
-    for (qtd = 0; qtd < 2; qtd++)
+    maiores[qtd] = M[0][0];
+    maior = M[0][0];
+    posicoes[qtd] = 0;
+    posicoes[qtd + 1] = 0;
+
+    printf("foi!");
+
+    for (k = 0; k < 3; k++)
     {
-        for (k = 0; k < l; k++)
+        for (i = 0; i < l; i++)
         {
-            for (p = 0; p < c; p++)
+            for (j = 0; j < c; j++)
             {
-                for (i = 0; i < l; i++)
+                if (M[i][j] > maior)
                 {
-                    for (j = 0; j < c; j++)
-                    {
-                        if (M[k][p] > M[l][c])
-                        {
-                            maior++;
-                        }
+                    if (tresMaiores(M, l, c, M[i][j]))
+                    { // Guarda elemento e posição
+                        maior = M[i][j];
+                        maiores[qtd] = maior;
+                        posicoes[qtd] = i;
+                        posicoes[qtd + 1] = j;
+
+                        qtd++;
                     }
                 }
             }
         }
-        // if maior >
     }
+
+    printf("\nfui.\n");
+    printaVet(maiores, qtd);
+}
+
+int tresMaiores(int **M, int l, int c, int maior)
+{
+    int i, j, outrosMaiores;
+
+    for (i = 0; i < l; i++)
+    {
+        for (j = 0; j < c; j++)
+        {
+            if (maior < M[i][j])
+            {
+                outrosMaiores++;
+            }
+        }
+    }
+
+    if (outrosMaiores < 3)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+void transpoeDinamica(int **mOriginal, int **mTransposta, int l, int c)
+{
+    int i, j;
+
+    for (i = 0; i < l; i++)
+    {
+        for (j = 0; j < c; j++)
+        {
+            mTransposta[i][j] = mOriginal[j][i];
+        }
+    }
+}
+
+void printaVet(int V[], int t)
+{
+    int i;
+
+    printf("\n");
+    for (i = 0; i < t; i++)
+    {
+        printf("%i ", V[i]);
+    }
+    printf("\n");
 }
 
 void printaMat(int **M, int l, int c)
--- lista8jordana/Ex122.c
+++ lista8jordana/Ex122.c
@@ -7,4 +7,223 @@ manipular a matriz:
 (c) Informar um nome, procurar a linha onde ele se encontra e substituir por outro
 nome;
 (d) Informar um nome, procurar a linha onde ele se encontra e apagar;
-(e) Pedir para recuperar o nome contido em uma linha da matriz; */
+(e) Pedir para recuperar o nome contido em uma linha da matriz;
+(f)* Sair */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char *alocaLinhas(int l)
+{
+    char *linha;
+
+    linha = (char *)malloc(l * sizeof(char));
+    return linha;
+}
+
+char **mallocaMatriz(int l, int c)
+{
+    int i;
+    char **matriz;
+
+    matriz = (char **)malloc(c * sizeof(char *));
+    for (i = 0; i < c; i++)
+    {
+        matriz[i] = alocaLinhas(l);
+    }
+
+    return matriz;
+}
+
+// Para retirar lixo das linhas não inicializadas (sem nome)
+// to triste pq n funcionou
+void inicializaMat(char **matriz, int l)
+{
+    int jordana;
+
+    for (jordana = 0; jordana < l; jordana++)
+    {
+        memset(matriz[jordana], 0, 30); // demorei um pouco pra pensar nisso talkei e talvez esteja errado
+    }
+}
+
+// a. Usa fgets para escrever o nome no vetor da linha indicada
+void escreveNome(char **matriz, int maxNomes)
+{
+    int linhaAlvo;
+
+    printf("Escrever nome em qual linha? (Linha maxima: %i)\n", maxNomes);
+    scanf("%i", &linhaAlvo);
+
+    while (linhaAlvo <= 0 || linhaAlvo > maxNomes)
+    {
+        printf("Por favor, escolha uma linha entre 1 e %i. (Voce escolheu %i)\n", maxNomes, linhaAlvo);
+        scanf("%i", &linhaAlvo);
+    }
+
+    getchar(); // scanf diretamente acima atrapalha o fgets
+    printf("Informe o nome:\n");
+    fgets(matriz[linhaAlvo - 1], 30, stdin);
+}
+
+// b. Usa memset para zerar o vetor do nome na linha indicada)
+void apagaNome(char **matriz, int maxNomes)
+{
+    int linhaAlvo;
+
+    printf("Apagar nome de qual linha? (Linha maxima: %i)\n", maxNomes);
+    scanf("%i", &linhaAlvo);
+
+    while (linhaAlvo <= 0 || linhaAlvo > maxNomes)
+    {
+        printf("Por favor, escolha uma linha entre 1 e %i. (Voce escolheu %i)\n", maxNomes, linhaAlvo);
+        scanf("%i", &linhaAlvo);
+    }
+
+    memset(matriz[linhaAlvo - 1], 0, 30);
+}
+
+// c, d. Usa strcmp para checar se o nome está em alguma das linhas e retorna o nº da linha
+int procuraNome(char **matriz, int maxNomes)
+{
+    int i;
+    char *string;
+
+    string = (char *)malloc(30 * sizeof(char));
+
+    getchar();
+    printf("Informe o nome:\n");
+    fgets(string, 30, stdin);
+
+    for (i = 0; i < maxNomes; i++)
+    {
+        if (strcmp(string, matriz[i]) == 0)
+        {
+            return i;
+        }
+    }
+
+    printf("O nome nao foi encontrado na matriz.\n");
+
+    return -1;
+}
+
+int procuraSubstituiNome(char **matriz, int maxNomes)
+{
+    int linhaAlvo;
+    linhaAlvo = procuraNome(matriz, maxNomes);
+    if (linhaAlvo == -1) // Caso o nome não exista na matriz
+    {
+        return 0;
+    }
+
+    printf("Informe o novo nome:\n");
+    fgets(matriz[linhaAlvo], 30, stdin);
+
+    return 0;
+}
+
+int procuraApagaNome(char **matriz, int maxNomes)
+{
+    int linhaAlvo;
+    linhaAlvo = procuraNome(matriz, maxNomes);
+    if (linhaAlvo == -1) // Caso o nome não exista na matriz
+    {
+        return 0;
+    }
+
+    memset(matriz[linhaAlvo], 0, 30);
+
+    return 0;
+}
+
+// e. Seleciona a linha do nome e printa caractere por caractere
+void printaNome(char **matriz, int maxNomes)
+{
+    int i, linhaAlvo;
+
+    printf("Exibir nome de qual linha? (Linha maxima: %i)\n", maxNomes);
+    scanf("%i", &linhaAlvo);
+
+    while (linhaAlvo <= 0 || linhaAlvo > maxNomes)
+    {
+        printf("Por favor, escolha uma linha entre 1 e %i. (Voce escolheu %i)\n", maxNomes, linhaAlvo);
+        scanf("%i", &linhaAlvo);
+    }
+
+    for (i = 0; matriz[linhaAlvo - 1][i] != '\0'; i++)
+    {
+        printf("%c", matriz[linhaAlvo - 1][i]);
+    }
+}
+
+void menuGaludo(char **nomes, int maxNomes)
+{
+    char op;
+
+    do
+    {
+        printf("\n-- MATRIZ DE NOMES --\n");
+        printf("------ OPCOES: ------\n\n");
+        printf("[a] Gravar um nome em uma linha da matriz\n");
+        printf("[b] Apagar o nome contido em uma linha da matriz\n");
+        printf("[c] Informar um nome, procurar a linha onde ele se encontra e substituir por outro nome\n");
+        printf("[d] Informar um nome, procurar a linha onde ele se encontra e apagar\n");
+        printf("[e] Pedir para recuperar o nome contido em uma linha da matriz\n");
+        printf("[f] Sair\n");
+
+        scanf(" %c", &op);
+
+        switch (op)
+        {
+        case 'a':
+            escreveNome(nomes, maxNomes);
+            break;
+
+        case 'b':
+            apagaNome(nomes, maxNomes);
+            break;
+
+        case 'c':
+            procuraSubstituiNome(nomes, maxNomes);
+            break;
+
+        case 'd':
+            procuraApagaNome(nomes, maxNomes);
+            break;
+
+        case 'e':
+            printaNome(nomes, maxNomes);
+            break;
+
+        case 'f': // Para não dar erro no 'do while'
+            break;
+
+        default:
+            printf("Entrada invalida (%c)! Tente novamente.\n", op);
+        }
+
+    } while (op != 'f');
+}
+
+int main()
+{
+    int maxNomes;
+    char **nomes;
+
+    printf("Informe o numero maximo de nomes na matriz:\n");
+    scanf("%i", &maxNomes);
+
+    nomes = mallocaMatriz(maxNomes, 30);
+    // inicializaMat(nomes, maxNomes);
+
+    menuGaludo(nomes, maxNomes);
+
+    for (int i = 0; i < maxNomes; i++)
+    {
+        free(nomes[i]);
+    }
+    free(nomes);
+    return 0;
+}
